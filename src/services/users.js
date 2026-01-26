c01874dd8f8e682fe273930d4536654f4241c9e4
MiguelangelRestrepo <migue200707@gmail.com>
2026-01-26 11:22:32 -0500

    JSON Server

--- src/services/users.js
+++ src/services/users.js
@@ -12,9 +12,51 @@ export const getUsersById = async (id) => {
     return response;
 }
 
-// export const getUsersById = async (id) => {
-//     const data = await fetch(`http://localhost:3000/usuarios/${id}`)
-//     const response = await data.json()
+export const deleteUsersById = async (id) => {
+    const data = await fetch(`http://localhost:3000/usuarios/${id}`,{method: 'DELETE'}).then(response => response.json())
+    .then(data => console.log('Usuario eliminado:', data));
 
-//     return response;
-// }
+    return "Usuario eliminado";
+}
+
+export const createUser = async () => {
+    const data = await fetch(`http://localhost:3000/usuarios`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: 4,
+            nombre: "Juan Santamaria",
+            email: "juan@example.com",
+            edad: 21,
+            ciudad: "Medellín"
+        })
+        }).then(response => response.json())
+        .then(data => console.log('Registro creado:', data))
+        .catch(error => console.error('Error:', error));
+
+
+    return "usuario creado con exito";
+}
+
+export const updateUser = async (id) => {
+    const data = await fetch(`http://localhost:3000/usuarios/${id}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: 4,
+            nombre: "Juan David",
+            email: "juan@example.com",
+            edad: 21,
+            ciudad: "Medellín"
+        })
+        }).then(response => response.json())
+        .then(data => console.log('Registro creado:', data))
+        .catch(error => console.error('Error:', error));
+
+
+    return "usuario actualizado con exito";
+}
